65b55944a705db2122763a8ac41f90b998bec39f
Josh Pullin <joshpullin134@gmail.com>
2016-06-08 11:38:59 -0400

    Update app.js

--- app.js
+++ app.js
@@ -47,4 +47,27 @@ var main = function() {
     });
 }
 
+var autoSlide = setInterval(function() {
+    var currentSlide = $('.active-slide');
+    var nextSlide = currentSlide.next();
+        
+    if(nextSlide.length == 0) {
+        var nextSlide = $('.slide').first();
+    }
+        
+    currentSlide.fadeOut(600).removeClass('active-slide');
+    nextSlide.fadeIn(600).addClass('active-slide');
+
+        
+    var currentDot = $('.active-dot');
+    var nextDot = currentDot.next();
+        
+    if(nextDot.length == 0) {
+        var nextDot = $('.dot').first();
+    }
+        
+    currentDot.removeClass('active-dot');
+    nextDot.addClass('active-dot');
+}, 4000);
+
 $(document).ready(main);
